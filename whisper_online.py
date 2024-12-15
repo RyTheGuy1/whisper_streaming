552bb93d5d9a00b2a8c6796845f99e37a491396c
Ryan Kelly <lryannathaniel@gmail.com>
2024-12-14 20:05:18 -0500

    Updated model size + path

--- whisper_online.py
+++ whisper_online.py
@@ -120,11 +120,11 @@ class FasterWhisperASR(ASRBase):
 
         # or run on GPU with INT8
         # tested: the transcripts were different, probably worse than with FP16, and it was slightly (appx 20%) slower
-        model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
+        model = WhisperModel(model_size_or_path, device="cuda", compute_type="int8_float16")
 
         # or run on CPU with INT8
         # tested: works, but slow, appx 10-times than cuda FP16
-#        model = WhisperModel(modelsize, device="cpu", compute_type="int8") #, download_root="faster-disk-cache-dir/")
+#        model = WhisperModel(model_size_or_path, device="cpu", compute_type="int8") #, download_root="faster-disk-cache-dir/")
         return model
 
     def transcribe(self, audio, init_prompt=""):
